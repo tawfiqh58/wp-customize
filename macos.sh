783c2b4667ed0af39116193b277bd10b57f5b4b8
Tawfiq Hasan <58129729+tawfiqh58@users.noreply.github.com>
2024-07-10 09:24:49 +0600

    Update macos.sh

--- macos.sh
+++ macos.sh
@@ -1,2 +1,4 @@
 # Fix permission issue on MacOS for XAMPP site
 $ sudo chown -R www-data:www-data /path/to/your/wordpress/directory
+# or
+$ sudo chown -R daemon:daemon /path/to/your/wordpress/directory
