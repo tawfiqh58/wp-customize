9373a5d74cb58c4675b05a0b59957ab2f357537a
Tawfiq Hasan <58129729+tawfiqh58@users.noreply.github.com>
2024-07-31 10:26:34 +0600

    Update settings.sql

--- elementor/settings.sql
+++ elementor/settings.sql
@@ -9,3 +9,9 @@ SELECT post_id, meta_value
 FROM wp_postmeta
 WHERE meta_key = '_elementor_page_settings'
   AND post_id IN (SELECT ID FROM wp_posts WHERE post_status = 'publish');
+
+-- update value (example)
+UPDATE wp_postmeta
+SET meta_value = 'a:1:{s:5:"color";s:7:"#FFFFFF";}'
+WHERE post_id = 6
+  AND meta_key = '_elementor_page_settings';
