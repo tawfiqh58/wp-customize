a7c61117c6f4416e8e4de9661611a1eee4ddda7a
Tawfiq Hasan <58129729+tawfiqh58@users.noreply.github.com>
2024-07-31 09:44:31 +0600

    Create elementor settings.sql

---
+++ elementor/settings.sql
@@ -0,0 +1,11 @@
+-- WP Data Access
+SELECT ID FROM wp_posts WHERE post_title = 'Default Kit' AND post_status = 'publish';
+
+-- Find the value of publish settings (post_id=previous-output)
+SELECT meta_value FROM wp_postmeta WHERE post_id = 6 AND meta_key = '_elementor_page_settings';
+
+-- or single query (**make sure you have updated the color at least once**)
+SELECT post_id, meta_value
+FROM wp_postmeta
+WHERE meta_key = '_elementor_page_settings'
+  AND post_id IN (SELECT ID FROM wp_posts WHERE post_status = 'publish');
